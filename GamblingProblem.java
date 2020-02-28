7f17554dc8062982b417fd6cbc065018955f953f
prathamesh <prathmesh6464@gmail.com>
2020-02-28 10:42:56 +0530

    MODIFIED : Added win or loose feature .

--- GamblingProblem.java
+++ GamblingProblem.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class GamblingProblem 
 {
 	public static void main(String[] args)
@@ -8,9 +9,28 @@ public class GamblingProblem
 		
 		//VARIABLES
 		int stake = 100;
+		int winCount = 0;
+		int looseCount = 0;
 		
 		
-		//DISPLAY VARIABLES
-		System.out.println("Stake : "+stake+"\n"+"Bet   : "+BET);
+		//METHOD OF WIN OR LOOSE USING RANDOM FUNCTION
+		Random randomValue = new Random();
+		int turnResult = randomValue.nextInt(2);
+		if(turnResult == 1) 
+		{
+		    System.out.println("Head");
+		    winCount++;
+		    stake++;
+		    System.out.println("Stake : "+stake);
+		}
+		else
+		{
+			System.out.println("Tail");
+			looseCount++;
+			stake--;
+			System.out.println("Stake : "+stake);
+		}
+		System.out.println("Win Count : "+winCount);
+		System.out.println("Loose Count : "+looseCount);
 	}
 }
