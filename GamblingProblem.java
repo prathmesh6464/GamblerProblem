eaad9dad5651509c89048fdd7c4f6ac51e281893
prathamesh <prathmesh6464@gmail.com>
2020-02-28 12:40:34 +0530

    MODIFIED : Added 20 days won and lost amount feature.

--- GamblingProblem.java
+++ GamblingProblem.java
@@ -8,42 +8,45 @@ public class GamblingProblem
 		int RESIGN_WON = 150;
 		int RESIGN_LOST = 50;
 
+
 		//VARIABLES
 		int stake = 100;
-		int winCount = 0;
-		int looseCount = 0;
+		int totalAmountAfter20DaysWon = 0;
+		int totalAmountAfter20DaysLost = 0;
+
 
 		//METHOD OF WIN OR LOOSE USING RANDOM FUNCTION
-		while(true)
+		for(int iter=0;iter<20;iter++)
 		{
-			Random randomValue = new Random();
-			int turnResult = randomValue.nextInt(2);
-			if(turnResult == 1) 
-			{
-				System.out.println("Head");
-				winCount++;
-				stake++;
-				System.out.println("Stake : "+stake);
-				if(RESIGN_WON == stake) 
+			while(true)
+			{ 
+				Random randomValue = new Random();
+				int turnResult = randomValue.nextInt(2);
+				if(turnResult == 1) 
 				{
-					System.out.println("Resign And Current Balance is : "+stake);	
-					break;
+					stake++;
+					if(RESIGN_WON == stake) 
+					{
+						totalAmountAfter20DaysWon += stake;
+						System.out.println("Total Amount Win  : "+totalAmountAfter20DaysWon);
+						break;						
+					}					
 				}
-			}
-			else
-			{
-				System.out.println("Tail");
-				looseCount++;
-				stake--;
-				System.out.println("Stake : "+stake);
-				if(RESIGN_LOST == stake) 
+				else
 				{
-					System.out.println("Resign And Current Balance is : "+stake);
-					break;
-				}
+					stake--;
+					if(RESIGN_LOST == stake) 
+					{
+						totalAmountAfter20DaysLost += stake;
+						System.out.println("Total Amount Lost : "+totalAmountAfter20DaysLost);
+						break;
+					}					
+				}				
 			}
-			System.out.println("Win Count   : "+winCount);
-			System.out.println("Loose Count : "+looseCount);
+			stake = 100;
 		}
+		//SHOWING ACCOUNT BALANCE AFTER 20 DAYS
+		System.out.println("Acount Balance After 20 Days(Won)       : "+totalAmountAfter20DaysWon);
+		System.out.println("Loose Count Balance After 20 Days(Lost) : "+totalAmountAfter20DaysLost);
 	}
 }
