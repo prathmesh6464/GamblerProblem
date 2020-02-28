023a34a81a4c875b8bc9e56e75674f491a7cdf72
prathamesh <prathmesh6464@gmail.com>
2020-02-28 11:33:32 +0530

    MODIFIED : Added resign feature is added.

--- GamblingProblem.java
+++ GamblingProblem.java
@@ -5,32 +5,45 @@ public class GamblingProblem
 	{
 		//CONSTANT VARIABLE
 		int BET = 1;
-
+		int RESIGN_WON = 150;
+		int RESIGN_LOST = 50;
 
 		//VARIABLES
 		int stake = 100;
 		int winCount = 0;
 		int looseCount = 0;
 
-
 		//METHOD OF WIN OR LOOSE USING RANDOM FUNCTION
-		Random randomValue = new Random();
-		int turnResult = randomValue.nextInt(2);
-		if(turnResult == 1) 
+		while(true)
 		{
-			System.out.println("Head");
-			winCount++;
-			stake++;
-			System.out.println("Stake : "+stake);
+			Random randomValue = new Random();
+			int turnResult = randomValue.nextInt(2);
+			if(turnResult == 1) 
+			{
+				System.out.println("Head");
+				winCount++;
+				stake++;
+				System.out.println("Stake : "+stake);
+				if(RESIGN_WON == stake) 
+				{
+					System.out.println("Resign And Current Balance is : "+stake);	
+					break;
+				}
+			}
+			else
+			{
+				System.out.println("Tail");
+				looseCount++;
+				stake--;
+				System.out.println("Stake : "+stake);
+				if(RESIGN_LOST == stake) 
+				{
+					System.out.println("Resign And Current Balance is : "+stake);
+					break;
+				}
+			}
+			System.out.println("Win Count   : "+winCount);
+			System.out.println("Loose Count : "+looseCount);
 		}
-		else
-		{
-			System.out.println("Tail");
-			looseCount++;
-			stake--;
-			System.out.println("Stake : "+stake);
-		}
-		System.out.println("Win Count : "+winCount);
-		System.out.println("Loose Count : "+looseCount);
 	}
 }
